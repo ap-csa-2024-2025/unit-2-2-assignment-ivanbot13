9b3a7c4818e62aa57cd7ea25296e8c7b79bac1b0
ivanbot13 <ikduong100@student.hbuhsd.edu>
2024-10-01 11:09:48 -0700

    shopping

--- Classwork.java
+++ Classwork.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Classwork
 {
   public static void main(String[] args)
   {
-    // write your code
+    Scanner sc = new Scanner(System.in);
+
+    System.out.println("What type of item are you buying?");
+    String item = sc.nextLine();
+    System.out.println("How many are you buying?");
+    int amt = sc.nextInt();
+    System.out.println("How much does each one weigh?");
+    double weight = sc.nextDouble();
+    double total = amt * weight;
+    System.out.println("So " + amt + " " + item + " at " + weight + " pounds each will weigh " + total + " pounds total");
   }
 }
